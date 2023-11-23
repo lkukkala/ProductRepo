a48ef4902f4058029e484798e7a152681f8ed219
lkukkala <149366339+lkukkala@users.noreply.github.com>
2023-11-23 12:17:32 +0530

    Add files via upload

---
+++ 01-springboot-product-ms-get/src/main/java/com/cg/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.cg.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.Product;
+import com.cg.service.ProductService;
+
+import jakarta.validation.Valid;
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	@Autowired
+	ProductService service;
+	@GetMapping("/hello")
+	public String printHello() {
+		return "Hii Spring";
+	}
+	
+//	@GetMapping("/products")
+	//@GetMapping(path="/products",produces= {MediaType.APPLICATION_JSON_VALUE})
+	//@GetMapping(path="/products",produces= {MediaType.APPLICATION_ATOM_XML_VALUE})
+	public List<Product>viewProductList() {
+		System.out.println("Helo");
+		List<Product> listproducts = service.listAll();
+		return listproducts;
+	}
+	@GetMapping("/products/{pid}")
+		public Optional<Product> findByProduct(@PathVariable int pid) {
+			return service.getProduct(pid);
+		}
+	@PostMapping("/products")
+	public Product createEmployee(@Valid @RequestBody Product newProduct) {
+		return service.createProduct(newProduct);
+	}
+	@DeleteMapping("/products/{id}")
+	public Map<String, Boolean> deleteProduct(@PathVariable(value = "id")Integer productId){
+		return service.deleteProduct(productId);
+	}
+	@PutMapping("/products/{id}")
+	public String updateProduct(@PathVariable(value = "id")Integer id,@Valid @RequestBody Product newProduct){
+		return service.updateProduct(id,newProduct);
+	}
+	@Query("select upper(name) from prodcut")
+	public String convertToUpperCase() {
+		return "";
+		}
+	
+	
+}
